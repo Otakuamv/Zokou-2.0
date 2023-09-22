6e2a74931931c988e5c2c20aefb3bd7ca00662df
Otakuamv <102306088+Otakuamv@users.noreply.github.com>
2023-09-22 11:09:40 +0200

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *Zokou* \n\n ' + 'je suis un bot Whatsapp Multi-appareil ';
+    let z = 'Salut je m\'appelle *조빈스* \n\n ' + 'je suis un bot Whatsapp Multi-appareil ';
     let d = ' developpé par *Djalega++*';
     let varmess = z + d;
-    var img = 'https://wallpapercave.com/uwp/uwp3842939.jpeg';
+    var img = 'https://wallpapercave.com/uwp/uwp4009031.jpeg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
