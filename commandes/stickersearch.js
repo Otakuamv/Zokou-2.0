5dc5cf90ba29efa8d05736523ce3654c03acade9
Otakuamv <102306088+Otakuamv@users.noreply.github.com>
2023-09-22 10:53:03 +0200

    Update stickersearch.js

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -5,7 +5,7 @@ const {zokou} = require("../framework/zokou");
 zokou({
   nomCom: "stickersearch",
   categorie: "Recherche",
-  reaction: "🍁"
+  reaction: "🌸"
 },
 async (dest, zk, commandeOptions) => {
   const { repondre, ms, arg, nomAuteurMessage } = commandeOptions;
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'Zokou-MD',
+      author: '조빈스🌸',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
