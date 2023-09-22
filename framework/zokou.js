d4ccc4568f87de80d4dc18d53e6e0cef318efa1e
Otakuamv <102306088+Otakuamv@users.noreply.github.com>
2023-09-22 01:55:30 +0100

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "Général";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🪰";
+        infoComs.reaction = "🌸";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
